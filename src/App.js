173def4e403d0501f7c2631d17f5e5d39b7332ae
Nate Tan <nktan93@gmail.com>
2019-11-05 22:18:55 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
 			<div className="App">
 				<CatComponent />
 				<EinsteinQuoteComponent />
+				<MouseComponent />
 				{/* one more component missing */}
 			</div>
 		);
